6b47b6525772d4a4327094b7eec5542f57e26713
Sebastián Badea <sebastian.badea@infojobs.net>
2016-08-29 07:32:30 +0200

    Updated functions bsl file to add reference to the actual function definition examples file

--- 1a.06.BSL - Function definitions.rkt
+++ 1a.06.BSL - Function definitions.rkt
@@ -1,3 +1,4 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1a.05.BSL - Function definitions|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1a.06.BSL - Function definitions|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; check out function-definitions-starter file
