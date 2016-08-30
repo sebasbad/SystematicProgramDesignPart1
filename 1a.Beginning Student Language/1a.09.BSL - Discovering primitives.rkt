33cb3bea63cf01ba6cb8c8a315bc68929de23eaa
Sebastián Badea <sebastian.badea@infojobs.net>
2016-08-30 16:58:07 +0200

    Added discovering primitives bsl file

---
+++ 1a.Beginning Student Language/1a.09.BSL - Discovering primitives.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |1a.09.BSL - Discovering primitives|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;(circle 10 "solid" "red")
+;(rectangle 30 60 "outline" "blue")
+;(text "hello" 24 "orange")
+
+(triangle 40 "solid" "purple")
+
+(overlay (circle 10 "solid" "red")
+         (circle 20 "solid" "yellow")
+         (circle 30 "solid" "green"))
+
+(round (/ 3 4))
+
+(radial-star 8 8 64 "solid" "darkslategray")
+
+(radial-star 32 30 40 "outline" "black")
+
